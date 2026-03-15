58f674ca003653d3b9dc24d294ffb791be787d36
Anastasia-D-Kravchenko <unicornavna@gmail.com>
2026-03-15 16:39:41 +0100

    covered coercing functions and types

---
+++ Coercing.R
@@ -0,0 +1,28 @@
+class(1.7)
+
+xx <- c(1.7, "a")
+xx
+
+xx <- c(TRUE, 2)
+xx
+
+as.character(1.7)
+
+xx <- c(as.character(1.7), "a")
+xx
+
+as.numeric(TRUE)
+
+as.numeric(FALSE)
+
+as.logical("a")
+
+as.logical(1)
+
+as.logical(2)
+
+as.logical(100)
+
+as.logical(0)
+
+as.logical(-1)
