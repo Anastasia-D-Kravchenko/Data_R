dc09e138a915690dc39e3ed99555854720c32b3a
Anastasia-D-Kravchenko <unicornavna@gmail.com>
2026-03-15 16:42:56 +0100

    covered naming

---
+++ Naming.R
@@ -0,0 +1,26 @@
+X <- 1:3
+X
+
+print(X)
+length(X)
+anyNA(X)
+names(X)
+
+letters
+
+LETTERS
+
+LETTERS[1:10]
+
+names(X) <- LETTERS[1:3]
+X
+
+names(X)
+
+names(X) <- letters[1:3]
+X
+
+names(X)
+
+a <- c("d" = 4, "e" = 5, "f" = 6)
+a
